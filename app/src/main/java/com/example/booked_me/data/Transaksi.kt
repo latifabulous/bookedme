2f596603a10124333c5d7f4aaf75e338b165f29a
Ni Nyoman Wahyuni Indraswari <nunikyuni0300@gmail.com>
2021-06-25 02:42:57 +0800

    be cart update

--- app/src/main/java/com/example/booked_me/data/Transaksi.kt
+++ app/src/main/java/com/example/booked_me/data/Transaksi.kt
@@ -3,7 +3,7 @@ package com.example.booked_me.data
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
-<<<<<<< HEAD
+
 @Parcelize
 data class Transaksi(
     var judul_buku : String? = null,
@@ -13,18 +13,5 @@ data class Transaksi(
     var alamat_user : String? = null,
     var date : String? = null,
     var gambar : String? = null
-=======
 
-
-@Parcelize
-data class Transaksi (
-    var judul_buku : String? = null,
-    var harga: String?= null,
-    var user : String? = null,
-    var phone:String? =null,
-    var alamat_user:String? = null,
-    var date:String? = null,
-    var status:String? = null
-
->>>>>>> cd8f7b77531fbbc028228652035a54abd01bca5b
 ) : Parcelable
