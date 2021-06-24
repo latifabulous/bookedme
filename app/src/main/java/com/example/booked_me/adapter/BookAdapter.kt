01d53eadfb28390acc22922a19881295d829f0f1
latifa <n.latifab@gmail.com>
2021-06-24 12:44:41 +0800

    add listener to book adapter

--- app/src/main/java/com/example/booked_me/adapter/BookAdapter.kt
+++ app/src/main/java/com/example/booked_me/adapter/BookAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.booked_me.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import com.bumptech.glide.Glide
 import com.example.booked_me.R
 import com.example.booked_me.data.Book
 import com.example.booked_me.databinding.ItemListBinding
+import com.example.booked_me.presentation.DetailBookActivity
 import com.example.booked_me.uitel.loadImage
 import com.squareup.picasso.Picasso
 
@@ -26,6 +28,12 @@ class BookAdapter(var context : Context, var listBook: List<Book>) : RecyclerVie
 //                Glide.with(itemView.context)
 //                    .load(book.gambar)
 //                    .into(imgBookCover)
+
+                itemView.setOnClickListener {
+                    val intent = Intent(itemView.context, DetailBookActivity::class.java)
+                    intent.putExtra("EXTRA_DATA", book)
+                    itemView.context.startActivity(intent)
+                }
 
             }
         }
